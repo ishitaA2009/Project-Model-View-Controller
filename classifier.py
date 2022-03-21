e459f0c448f4a58d2ccf0c8fe9c73dda86db35e5
ishitaA2009 <78198031+ishitaA2009@users.noreply.github.com>
2022-03-21 21:25:25 +0530

    Add files via upload

---
+++ classifier.py
@@ -0,0 +1,48 @@
+import cv2
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.datasets import fetch_openml
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+from PIL import Image
+import PIL.ImageOps
+import os, ssl, time
+from flask import Flask,jsonify, request
+
+X = np.load('image.npz')['arr_0']
+y = pd.read_csv("labels.xlsx")["labels"]
+print(pd.Series(y).value_counts())
+classes = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+n_classes = len(classes)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=9, train_size=7500, test_size=2500)
+#so that all X,y points are equal
+X_train_scaled = X_train/255.0
+X_test_scaled = X_test/255.0
+ 
+clf = LogisticRegression(solver='saga', multi_class='multinomial').fit(X_train_scaled, y_train)
+
+def get_prediction(image):
+    im_pil = Image.open(image)
+ 
+    image_bw = im_pil.convert('L')
+    image_bw_resized = image_bw.resize((28,28), Image.ANTIALIAS)
+
+    pixel_filter = 20
+    # covert data into scalar quantity
+    min_pixel = np.percentile(image_bw_resized, pixel_filter)
+    # limit the values between 0,255
+    image_bw_resized_scaled = np.clip(image_bw_resized-min_pixel, 0, 255)
+    # max number of numbers
+    max_pixel = np.max(image_bw_resized)
+
+    # converting to array
+    image_bw_resized_inverted_scaled = np.asarray(image_bw_resized_inverted_scaled)/max_pixel
+
+    #creating a teat sample and making a prediction
+    test_sample = np.array(image_bw_resized_inverted_scaled).reshape(1,784)
+    test_pred = clf.predict(test_sample)
+    return test_pred[0]
